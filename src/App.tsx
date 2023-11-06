ee9b637168728d720b8932b13888aa6ad27246e3
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-11-06 15:21:01 +0100

    add themes, ThemeProvider, ThemeContext, create hook useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss'
+import './styles/index.scss'
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
+
 
 export const App = () => {
+    const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to="/">Main</Link>
             <Link to="/about">About</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path="/" element={<MainPageAsync />}/>
-                    <Route path="/about" element={<AboutPageAsync />}/>
+                    <Route path="/" element={<MainPageAsync/>}/>
+                    <Route path="/about" element={<AboutPageAsync/>}/>
                 </Routes>
             </Suspense>
         </div>
